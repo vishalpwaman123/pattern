51b018024c641dd6cb057cf832621f2d0c844d4d
vishalpwaman <vishalpwaman1997@gmail.com>
2020-04-25 07:09:27 -0400

    usecase 2

--- email.sh
+++ email.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 read -p "Enter the postal Index Number :" pin;
-pattern="^abc\.{1}[a-z]{3}@bridgelabz\.{1}co\.{1}\w+$"
+pattern="^[a-zA-Z0-9.-]+@bridgelabz\.{1}\w+$"
 if [[ $pin =~ $pattern ]]
 then 
 	echo "valid";
